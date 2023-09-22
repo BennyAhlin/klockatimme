9b3eb8e898195ad4d95a56cc8eeeaa9a73dee06a
BennyAhlin <Benny.Ahlin@hotmail.com>
2023-09-22 11:06:59 +0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿
+int hour = 0;
+int sekund = 0;
+
+while (hour < 24)
+{
+
+    if (hour < 10)
+    {
+        Console.Write("0" + hour + ":");
+    }
+    else
+    {
+        Console.Write(hour + ":");
+    }
+    if (sekund < 10)
+    {
+        Console.WriteLine("0" + sekund);
+    }
+    else
+    {
+        Console.WriteLine(sekund);
+    }
+
+
+
+    if (sekund < 60)
+    {
+        sekund++;
+    }
+    if (sekund == 60)
+    {
+        hour++;
+        sekund = (0);
+    }
+}
+
+
